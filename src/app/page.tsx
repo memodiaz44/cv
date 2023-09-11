5e2b6aca11b9803743f819ff490d8ba178592a2d
Memo Diaz <121352998+memodiaz44@users.noreply.github.com>
2023-09-11 12:07:57 -0600

    routiing fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
+"use client"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation'
+
+
+
 
 
 export default function App() {
+  const router = useRouter();
 
+  useEffect(() => {
+    // Redirect to /sections/home when the root URL ("/") is accessed
+    router.push('/sections/home');
+  }, []);
 
-  return (
-    <>
-<h1>Hello, Next.js!</h1>;
-      </>
-  )
+  return null; // You can return null or any other content you want here
 }
