101fb3b84290d70f875272aa3afbd487466bfdef
Memo Diaz <121352998+memodiaz44@users.noreply.github.com>
2023-09-11 12:18:23 -0600

    fixed types

--- src/app/api/routes/route.ts
+++ src/app/api/routes/route.ts
@@ -16,9 +16,14 @@ export async function POST(request: Request) {
   const data: ContactForm = await request.json();
   console.log('data: ', data);
 
-  const { email, comment, subject } = data;
+  if (data.email) {
+    // Explicitly assert all properties as strings
+    const email = data.email as string;
+    const comment = data.comment as string;
+    const subject = data.subject as string;
 
-  await sendEmail(data);
+    await sendEmail({ email, comment, subject });
+  }
 
-  return NextResponse.json({ email, comment , subject });
+  return NextResponse.json({ email: data.email, comment: data.comment, subject: data.subject });
 }
